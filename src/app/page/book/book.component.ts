97a5a4b0f3881e612c85f5e19f265df01b941719
LuisVynicius <luismevy@gmail.com>
2024-10-30 21:06:21 -0300

    Created book-page

--- src/app/page/book/book.component.ts
+++ src/app/page/book/book.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ChapterBoxComponent } from './chapter-box/chapter-box.component';
+import { CommonModule } from '@angular/common';
+import { chapter } from '../../objects/chapter';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [
+    ChapterBoxComponent,
+    CommonModule
+  ],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
 export class BookComponent {
-
+  chaps: chapter[] = [
+    {
+      title: "O começo da aventura"
+    },
+    {
+      title: "O Goku aparece em algum lugar"
+    },
+    {
+      title: "O Goku está indo ali"
+    },
+    {
+      title: "Caramba, o Goku Morreu"
+    },
+    {
+      title: "O Goku revive e ganha, fim"
+    },
+  ];
 }
---
+++ src/app/page/book/chapter-box/chapter-box.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-chapter-box',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './chapter-box.component.html',
+  styleUrl: './chapter-box.component.css'
+})
+export class ChapterBoxComponent {
+  @Input()
+  chapNumber: string = "";
+  @Input()
+  chapTitle: string = "";
+}
