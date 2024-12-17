9f9d3db815a23c9ee767b41850942651825a55ca
LuisVynicius <luismevy@gmail.com>
2024-12-17 16:06:51 -0300

    Add Register page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { RectangleBackgroundComponent } from '../shared/rectangle-background/rectangle-background.component';
 import { InputBoxComponent } from './input-box/input-box.component';
 import { DefaultButtonComponent } from '../shared/default-button/default-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
     RectangleBackgroundComponent,
     InputBoxComponent,
-    DefaultButtonComponent
+    DefaultButtonComponent,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  hasAccount: boolean = true
 
+  switchAccount() {
+    this.hasAccount = !this.hasAccount;
+  }
 }
