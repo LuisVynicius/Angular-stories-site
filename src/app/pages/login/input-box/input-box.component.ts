8349612db21102da7f054e6d9d5e77c607e5898a
LuisVynicius <luismevy@gmail.com>
2025-04-01 19:00:51 -0300

    Update login page

--- src/app/pages/login/input-box/input-box.component.ts
+++ src/app/pages/login/input-box/input-box.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-box',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './input-box.component.html',
   styleUrl: './input-box.component.css'
 })
 export class InputBoxComponent {
   @Input()
   text: string = "";
+  @Input()
+  value: string = "";
+  @Input()
+  type: string = "text";
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  onValueChange(newValue: string): void {
+    this.value = newValue;
+    this.valueChange.emit(this.value);
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,9 @@ import { RectangleBackgroundComponent } from '../shared/rectangle-background/rec
 import { InputBoxComponent } from './input-box/input-box.component';
 import { DefaultButtonComponent } from '../shared/default-button/default-button.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { enviroment } from '../../config/environment';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +13,69 @@ import { CommonModule } from '@angular/common';
     RectangleBackgroundComponent,
     InputBoxComponent,
     DefaultButtonComponent,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  hasAccount: boolean = true
+  hasAccount: boolean = true;
+
+  hasAccountMessage: string = "Já possuo uma conta";
+
+  username: string = "";
+  email: string = "";
+  password: string = "";
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
 
   switchAccount() {
     this.hasAccount = !this.hasAccount;
+
+    if (this.hasAccount) {
+      this.hasAccountMessage = "Já possuo uma conta"
+    } else {
+      this.hasAccountMessage = "Não possuo uma conta"
+    }
+
+    this.username = "";
+    this.password = "";
+    this.email = "";
   }
+
+  register() {
+    this.http.post(enviroment.apiUrl + "/user/register", {
+      "username": this.username,
+      "email": this.email,
+      "password": this.password
+    }).subscribe({
+      next: (response) => {
+        this.switchAccount();
+      },
+      error: (err) => {
+        console.log(err.message)
+      }
+    });
+  }
+
+  login() {
+    this.http.post<{
+      Authorization: string
+    }>(enviroment.apiUrl + "/login", {
+      username: this.username,
+      password: this.password
+    }, {observe: 'response'}).subscribe({
+      next: (data) => {
+        const token = data.body?.['Authorization']; 
+        console.log("Deu sim " + token);
+      },
+      error: (err) => {console.log("Deu não")}
+    });
+  }
+
 }
